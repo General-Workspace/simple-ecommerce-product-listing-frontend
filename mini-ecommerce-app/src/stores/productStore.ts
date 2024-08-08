e074d0aa99b735e7d3c56b4f5e26cda5e41ee0fa
aniebietafia <aniebietafia87@gmail.com>
2024-08-08 17:48:58 +0100

    fix

--- mini-ecommerce-app/src/stores/productStore.ts
+++ mini-ecommerce-app/src/stores/productStore.ts
@@ -15,8 +15,7 @@ export const useProductStore = defineStore("product", () => {
   const error = ref<string | null>(null);
   const statusCode = ref<number | null>(null);
 
-  // const backendUrl = import.meta.env.VITE_BACKEND_URL;
-  const backendUrl = process.env.VITE_BACKEND_URL
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
   const fetchProducts = async () => {
     loading.value = true;
